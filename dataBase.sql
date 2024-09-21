03b28df0e184414ec93b18fe7236ee87d3856c4d
Vince <adesavince70@gmail.com>
2024-09-21 15:01:53 +0800

    Create dataBase.sql

---
+++ dataBase.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE task_manager;
+USE task_manager;
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    due_date_time DATETIME NOT NULL,
+    status VARCHAR(50) DEFAULT 'Pending'
+);
