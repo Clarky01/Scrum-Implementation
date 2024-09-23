5959446b38f64831c238c2f00103dbefdbb739a0
Vince <adesavince70@gmail.com>
2024-09-23 21:17:02 +0800

    Update createTask.js

--- createTask.js
+++ createTask.js
@@ -1,18 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('taskForm');
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault(); 
-
-        const taskName = document.getElementById('taskName').value;
-        const taskDescription = document.getElementById('taskDescription').value;
-        const dueDate = document.getElementById('dueDate').value;
-        const dueTime = document.getElementById('dueTime').value;
-
-        console.log(`Task Details:\nName: ${taskName}\nDescription: ${taskDescription}\nDue Date: ${dueDate}\nDue Time: ${dueTime}`);
-
-        window.location.href = 'tasklist.html'; 
-        
-        form.reset();
-    });
-});
